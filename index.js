699d08dd4ce8b7484ed092ebb1415bf5099dcccf
Labun <piotr.labunski@gmail.com>
2019-01-24 18:46:13 +0100

    FIXED: filter include

--- index.js
+++ index.js
@@ -8,14 +8,13 @@ module.exports = function (app, options) {
         limit: 50,
         order: '',
         where: {},
-        include: {},
+        include: {}
       };
       if (req.query.filter) {
         try {
           const fields = JSON.parse(req.query.filter);
           for (let field in fields) {
             if (fields.hasOwnProperty(field)) {
-              
               if (field === 'include') {
                 filter.include = fields[field];
                 return null;
@@ -54,52 +53,6 @@ module.exports = function (app, options) {
     }
     return null;
   };
-
-  // Set Content-Range for all search requests
-  const applyRange = function (ctx, next) {
-    if (!ctx.res._headerSent) {
-      let filter;
-      let limit = 50;
-      let skip = 0;
-      if (ctx.args && ctx.args.filter) {
-        filter = ctx.args.filter.where;
-        limit = ctx.args.filter.limit;
-        skip = ctx.args.filter.skip;
-      }
-
-      let name = this.pluralModelName || this.name;
-      this.count(filter, function (err, count) {
-        const last = Math.min(skip + limit, count);
-        ctx.res.set('Access-Control-Expose-Headers', 'Content-Range');
-        ctx.res.set('Content-Range', `${name.toLowerCase()} ${skip}-${last}/${count}`);
-        next();
-      });
-    } else {
-      next();
-    }
-  };
-
-  var pattern = options && Array.isArray(options.pattern) ? options.pattern : ['*.find'];
-  for (var i=pattern.length-1; i>=0; i--) {
-    remotes.after(pattern[i], applyRange);
-  }
-
-  app.middleware('routes:before', function (req, res, next) {
-    const filter = convertQuery(req);
-    if (filter) {
-      if (req.query) {
-        delete req.query.filter;
-        delete req.query.sort;
-        delete req.query.range;
-        req.query = {...req.query, filter: JSON.stringify(filter)};
-      } else {
-        req.query = {filter: JSON.stringify(filter)};
-      }
-    }
-    next();
-  });
-
-};
 
   // Set Content-Range for all search requests
   const applyRange = function (ctx, next) {
